fec8f12dff7c1337a5f0b895cf4eacb0082b8425
catsniper666 <35572754+catsniper666@users.noreply.github.com>
2018-02-08 18:53:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ var client = new Discord.Client();
 var bot = new Discord.Client();
 
 bot.on("ready", function() {
-    bot.user.setGame("::justice::")
+    bot.user.setGame("justice")
     console.log("Ready")
 });
 
@@ -30,7 +30,7 @@ bot.on("ready", function() {
 bot.on("guildMemberAdd", function(member) {
     member.guild.channels.find("name", "general").sendMessage(member.toString() + " Welcome in this little cancer my boi");
 
-    member.addRole(member.guild.roles.find("name", "guest"))
+    member.addRole(member.guild.roles.find("name", "og"))
 });
 
 
@@ -40,6 +40,10 @@ bot.on("message", function(message) {
     if (!message.content.startsWith(PREFIX)) return;
 
     var args = message.content.substring(PREFIX.length).split(" ");
+
+    let modlog = message.guild.channels.find('name', 'mod-log');
+
+    let modRole = message.guild.roles.find("name", "grimm commander");
 
     switch (args[0]) {
         case "ping":
@@ -70,7 +74,7 @@ bot.on("message", function(message) {
             mutedone.removeRole(Muted.id);
             message.reply(`**${mutedone.user.username}** was succesfully unmuted.`);
             break;
-        case "cat":
+        case "cat": 
             var mooma = new Discord.RichEmbed()
                 .setDescription("CatSniper is a fag");
             message.channel.sendEmbed(mooma);
@@ -91,39 +95,57 @@ bot.on("message", function(message) {
         case "muteinfo":
             if (!message.member.permissions.has("MANAGE_ROLES")) return message.reply("That command is only for mods... :utried: ");
             else message.reply("**mute {time} {user} *time must be specified in seconds***");
-            break;
+            break
         case "kick":
-            let modRole = message.guild.roles.find("name", "Grimm Commander");
-            if(!message.member.roles.find("name", "grimm commander")) 
-              return message.reply("You pleb, you don't have the permission to use this command.").catch(console.error);
-            if(message.mentions.users.size === 0) 
-              return message.reply("Please mention a user to kick").catch(console.error);
-            let kickMember = message.guild.member(message.mentions.users.first());
-            if(!kickMember) 
-              return message.reply("That user does not seem valid");
-            if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) 
-              return message.reply("I don't have the permissions (KICK_MEMBER) to do this.").catch(console.error);
-            kickMember.kick().then(member => message.reply(`**${member.user.username}** was kicked...`))
+        if(!message.member.roles.find("name", "grimm commander")) 
+          return message.reply("You pleb, you don't have the permission to use this command.").catch(console.error);
+        let reasonxx = args.slice(1).join(' ')
+        if(!reasonxx) 
+          return message.reply("You must provide a reason for the kick")
+        if(message.mentions.users.size === 0) 
+          return message.reply("Please mention a user to kick").catch(console.error);
+        let kickMember = message.guild.member(message.mentions.users.first());
+        if(!kickMember) 
+          return message.reply("That user does not seem valid");
+        if(!message.guild.member(bot.user).hasPermission("KICK_MEMBERS")) 
+          return message.reply("I don't have the permissions (KICK_MEMBER) to do this.").catch(console.error);
+        if (!modlog) return message.reply("There is no `mod-log` channel in this guild...")
+        kickMember.kick().then(member => message.reply(`**${member.user.username}** was kicked...`))
+            var kembed = new Discord.RichEmbed()
+              .setTitle("**KICK**")
+              .addField("**Kicked member:**", `${kickMember.username}`)
+              .addField("**Kicked member ID:**", `${kickMember.id}`)
+              .addField("**Moderator:**", `${message.author.username}`)
+              .addField("**Reason:**", `${reasonxx}`)
+        return message.guild.channels.get(modlog.id).sendEmbed(kembed)
             break;
         case "help":
+           var hembed = new Discord.RichEmbed()
+           .setTitle(`**All Available Moderation Commands**`)
+           .addField(`**Kick**`, `- kicks a member from the guild [usage]: *::kick <mentioned.user> <reason>*`)
+           .addField(`**Ban**`, `- bans a members from the guild [usage]: *::ban <mentioned.user> <reason>* `)
+           .addField(`**Prune**`, `- clears a desired amount of messages [usage]: *::prune <number.of.messages>*`)
+           .addField(`**Mute**`, `- mutes a member for a desired amount of minutes [usage]: *::mute <mentioned.user> <time>*`)
+           .addField(`**Unmute**`, `- unmutes a desired member [usage]: *::unmute <mentioned.user>*`)
+           .setFooter(`Requested by: ${message.author.username}`)
             if (!message.member.roles.find("name", "grimm commander"))
-              return message.reply("I can't provide any information for you because there are no available commands to use...");
-            message.reply("there's no available list of commands atm, however you can contact @catsniper for bot moderation details.");
+              return message.reply("I can't provide any information for you because there are no available commands to use...")
+              else message.channel.sendEmbed(hembed);
             break;
         case "warn": 
-            let reason = args.slice(1).join(' ');
             let user = message.mentions.users.first();
-            let modlog = message.guild.channels.find('name', 'mod-log');
+            let reason = args.slice(1).join(' ');
+            if (!message.member.roles.has(modRole.id)) return message.reply("You got no perms for that...")
             if (!modlog) return message.reply("There's no modlog channel in this server!");
-            if (reason.length < 1) return message.reply("You didn't provide any reason for you warn.");
-            if(message.mentions.users.size < 1) return message.reply("Mention someone to warn them!");
+            if (!user) return message.reply("Mention someone to warn them!");
+            if (!reason) return message.reply("You didn't provide any reason for you warn.");
             var warn = new Discord.RichEmbed()
             .setColor(0x00AE86)
             .addField('Action:', 'Warning:')
-            .addField('User:', `${user.username}#${user.discriminator}`)
-            .addField('Moderator:', `${message.author.username}#${message.author.dicriminator}`)
+            .addField('User:', `${user.username}`)
+            .addField('Moderator:', `${message.author.username}`)
             .addField("With reason:", `${reason}`)
-            return message.channel.sendEmbed(warn)
+            return message.guild.channels.get(modlog.id).send(warn);
             break;
         case "purge":
      
@@ -155,9 +177,10 @@ bot.on("message", function(message) {
         case "sr":
             let guest = message.mentions.users.first();
             let trole = args.slice(1).join(' ');
+            let therole = message.guild.roles.find("name", `${trole}`)
             if (!guest) return message.reply("Mention someone to give them a role!");
             if (!trole) return message.reply(`Type in the role you want to give!`)
-            guest.addRole("name", `${trole}`)
+            guest.addRole(therole.id)
             message.reply(`Succesfully added role ${trole} to user ${guest.user.username}`)
             break;
         case "invite":
@@ -166,6 +189,29 @@ bot.on("message", function(message) {
         case "invite fisher":
             message.channel.sendMessage("https://discordapp.com/api/oauth2/authorize?client_id=395292815309012992&scope=bot&permissions=1")
             break;
+        case "ban":
+        if(!message.member.roles.find("name", "grimm commander")) 
+        return message.reply("You pleb, you don't have the permission to use this command.").catch(console.error);
+      let reasonxxx = args.slice(1).join(' ')
+      if(!reasonxxx) 
+        return message.reply("You must provide a reason for the ban")
+      if(message.mentions.users.size === 0) 
+        return message.reply("Please mention a user to kick").catch(console.error);
+      let banMember = message.guild.member(message.mentions.users.first());
+      if(!banMember) 
+        return message.reply("That user does not seem valid");
+      if(!message.guild.member(bot.user).hasPermission("BAN_MEMBERS")) 
+        return message.reply("I don't have the permissions (BAN_MEMBER) to do this.").catch(console.error);
+      if (!modlog) return message.reply("There is no `mod-log` channel in this guild...")
+      banMember.ban().then(member => message.reply(`**${member.user.username}** was banned...`))
+          var kembed = new Discord.RichEmbed()
+            .setTitle("**KICK**")
+            .addField("**Kicked member:**", `${banMember.username}`)
+            .addField("**Kicked member ID:**", `${banMember.id}`)
+            .addField("**Moderator:**", `${message.author.username}`)
+            .addField("**Reason:**", `${reasonxxx}`)
+      return message.guild.channels.get(modlog.id).sendEmbed(kembed)
+           break;
         default:
             message.channel.sendMessage("CatSniper didn't create this command atm... Make sure to add it in #suggestions so our sexy boi can see!");
     }
